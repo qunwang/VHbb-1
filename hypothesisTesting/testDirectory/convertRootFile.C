5fc46f9e04dec2f8c72ae41a95fb5fcd82c94d0f
Benjamin <benjaminkreis@gmail.com>
2014-05-13 00:59:51 -0500

    REVERT require underscore to prevent 0M from changing 0Mf05ph0

--- hypothesisTesting/testDirectory/convertRootFile.C
+++ hypothesisTesting/testDirectory/convertRootFile.C
@@ -32,12 +32,12 @@ void convertRootFile(TString sigName = "Wh_125p6_0P", TString sigAltName = "Wh_1
     TString oldName = obj->GetName(); 
     TString newName = oldName;
     
-    if( oldName.Contains(sigName+"_") ) {
+    if( oldName.Contains(sigName) ) {
       newName.ReplaceAll(sigName,"sig");
       cout << oldName << " " << newName << endl;
       makeNew(myFile, oldName, newName);
     }
-    else if( oldName.Contains(sigAltName+"_") ) {
+    else if( oldName.Contains(sigAltName) ) {
       newName.ReplaceAll(sigAltName,"sig_ALT");
       cout << oldName << " " << newName << endl;
       makeNew(myFile, oldName, newName);

ccbcb4a09055e5869543bd49e2fb2fba462995b4
Benjamin <benjaminkreis@gmail.com>
2014-05-22 05:13:28 -0500

    allow various renormalizations

--- hypothesisTesting/testDirectory/convertRootFile.C
+++ hypothesisTesting/testDirectory/convertRootFile.C
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 #include "TFile.h"
 #include "TH1F.h"
@@ -7,17 +8,51 @@
 #include "TKey.h"
 #include "TObject.h"
 
+//
+// NOTE: Should be a little more clever with strings so that 0M and 0Mf05ph0 don't interfere
+//
+
 void makeNew(TFile * myFile, TString oldname, TString newname) {
-  
   myFile->cd();
   TH1F* h1 = (TH1F*)myFile->Get(oldname);
   TH1F* h2 = (TH1F*)h1->Clone(newname);
   //cout << oldname << " " << h1->Integral() << endl;
   h2->Write();
-
 }
 
-void convertRootFile(TString sigName = "Wh_125p6_0P", TString sigAltName = "Wh_125p6_0M") {
+void renormalize_to_other(TFile * myFile, TString integralHistName, TString changeHistName) {
+  cout << "Renormalizing " << changeHistName << endl;
+  myFile->cd();
+  TH1F* hIntegral = (TH1F*)myFile->Get(integralHistName);
+  TH1F* hChange = (TH1F*)myFile->Get(changeHistName);
+  hChange->Scale(hIntegral->Integral()/hChange->Integral());
+  hChange->Write();
+}
+
+void renormalize_by_ratio_of_hists(TFile * myFile, TString integralHistName_Numerator, TString integralHistName_Denominator, TString changeHistName) {
+  cout << "Renormalizing " << changeHistName << endl;
+  myFile->cd();
+  TH1F* hIntegral_N = (TH1F*)myFile->Get(integralHistName_Numerator);
+  //cout << hIntegral_N << endl;
+  TH1F* hIntegral_D = (TH1F*)myFile->Get(integralHistName_Denominator);
+  //cout << hIntegral_D << endl;
+  TH1F* hChange = (TH1F*)myFile->Get(changeHistName);
+  //cout << hChange << endl;
+  hChange->Scale(hIntegral_N->Integral()/hIntegral_D->Integral());
+  hChange->Write();
+}
+
+void renormalize_by_factor(TFile * myFile, double factor, TString changeHistName) {
+  cout << "Renormalizing " << changeHistName;
+  myFile->cd();
+  TH1F* hChange = (TH1F*)myFile->Get(changeHistName);
+  cout << setprecision(15) << " from " << hChange->Integral();
+  hChange->Scale(factor);
+  cout << setprecision(15) << " to " << hChange->Integral() << endl;
+  hChange->Write();
+}
+
+void convertRootFile(TString sigName = "Wh_125p6_0P", TString sigAltName = "Wh_125p6_0M", double altMu=-1.0) {
 
   TFile * myFile = new TFile("plots.root", "update");
   cout << "Changing file " << myFile << endl;
@@ -34,13 +69,20 @@ void convertRootFile(TString sigName = "Wh_125p6_0P", TString sigAltName = "Wh_1
     
     if( oldName.Contains(sigName) ) {
       newName.ReplaceAll(sigName,"sig");
-      cout << oldName << " " << newName << endl;
+      //cout << oldName << " " << newName << endl;
       makeNew(myFile, oldName, newName);
     }
     else if( oldName.Contains(sigAltName) ) {
+      if( sigAltName=="Wh_125p6_0M" && oldName.Contains("0Mf05ph0") ) continue; //dirty hack
       newName.ReplaceAll(sigAltName,"sig_ALT");
-      cout << oldName << " " << newName << endl;
+      //cout << oldName << " " << newName << endl;
       makeNew(myFile, oldName, newName);
+
+      //Renormalize by factor
+      if(altMu >= 0) {
+	renormalize_by_factor(myFile, altMu, newName);
+      }
+
     }
         
   }//end loop over keys
